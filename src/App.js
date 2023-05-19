025c7953735f22a97d4ace4ad913a9d13d39bf85
Chidders <64120293+Chiddersss@users.noreply.github.com>
2023-05-19 23:20:52 +0100

    catchup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css'; // Import a CSS file to style the agent cards
 
 function App() {
+  const [agents, setAgents] = useState([]);
+
+  useEffect(() => {
+    const getAgents = async () => {
+      try {
+        const response = await axios.get('https://valorant-api.com/v1/agents');
+        setAgents(response.data.data.slice(0, 18)); // set how many agents you want 
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getAgents();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="agent-cards-container">
+      {agents.map((agent) => (
+        <div
+          key={agent.uuid}
+          className="agent-card"
+          style={{
+            background: `url(${agent.fullPortrait}) center/cover, url(${agent.background}) center/cover`,
+            backgroundSize: '110%', // set background size to a larger value
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <div className="agent-info-overlay">
+            <h2>{agent.displayName}</h2>
+            <ul>
+              {agent.abilities.map((ability) => (
+                <li key={ability.slot}>
+                  {ability.displayName} - {ability.description}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
