1814bc1e6b9dd41f931d26fde0e8a6bad8c4ec62
Chidders <64120293+Chiddersss@users.noreply.github.com>
2023-05-19 23:27:42 +0100

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import './App.css'; // Import a CSS file to style the agent cards
+import './App.css';
 
 function App() {
   const [agents, setAgents] = useState([]);
@@ -9,13 +9,18 @@ function App() {
     const getAgents = async () => {
       try {
         const response = await axios.get('https://valorant-api.com/v1/agents');
-        setAgents(response.data.data.slice(0, 18)); // set how many agents you want 
+        setAgents(response.data.data.slice(0, 18));
       } catch (error) {
         console.log(error);
       }
     };
     getAgents();
   }, []);
+
+  const truncateDescription = (description, sentences) => {
+    const truncated = description.trim().split('.').slice(0, sentences).join('. ');
+    return truncated + '.';
+  };
 
   return (
     <div className="agent-cards-container">
@@ -25,15 +30,18 @@ function App() {
           className="agent-card"
           style={{
             background: `url(${agent.fullPortrait}) center/cover, url(${agent.background}) center/cover`,
-            backgroundSize: '110%', // set background size to a larger value
+            backgroundSize: '110%',
           }}
         >
           <div className="agent-info-overlay">
-            <h2>{agent.displayName}</h2>
+            <div className="agent-name">{agent.displayName}</div>
             <ul>
               {agent.abilities.map((ability) => (
                 <li key={ability.slot}>
-                  {ability.displayName} - {ability.description}
+                  <span className="ability-name">{ability.displayName}</span>
+                  <span className="ability-description">
+                    {truncateDescription(ability.description, 2)}
+                  </span>
                 </li>
               ))}
             </ul>
